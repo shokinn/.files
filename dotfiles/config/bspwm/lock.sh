670c77b06a4eeb5e4b8aa2d716ccb6532be7c6dc
Philip Henning <mail@philip-henning.com>
2018-01-02 18:05:44 +0100

    Lock KeePass when screen will be locked.

--- dotfiles/config/bspwm/lock.sh
+++ dotfiles/config/bspwm/lock.sh
@@ -1,5 +1,14 @@
+#
+# {{@@ env['dotdrop_warning'] @@}}
+#
+
 #!/bin/bash
-  
+
+# Set lock screen background
 lockscreen="$HOME/.config/bspwm/lock.png"
 
+# Lock all KeePass databases
+keepass --lock-all
+
+# Lock the screen
 i3lock -u -i $lockscreen
